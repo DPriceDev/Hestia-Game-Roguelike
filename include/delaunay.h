8dd7f5000f004db1a71667fb565ce5a128443163
DPriceDev <dpricedev@gmail.com>
2020-05-17 13:09:28 +0100

    updated delanauy to correctly generate the triangulation.

--- include/delaunay.h
+++ include/delaunay.h
@@ -12,32 +12,53 @@
 #include <maths/maths_types.h>
 #include <util/logger.h>
 
-struct Vertex;
-struct Triangle;
-
+/**
+ * Vertex
+ */
 struct Vertex {
-    HGE::Pointd x, y;
+    HGE::Pointf x, y;
 
-    Vertex(HGE::Pointd x, HGE::Pointd y) : x(x), y(y) { }
+    Vertex(HGE::Pointf x, HGE::Pointf y) : x(x), y(y) { }
     ~Vertex() = default;
 
     [[nodiscard]]
-    HGE::Pointd magnitude() const {
+    HGE::Pointf magnitude() const {
         return sqrt((x * x) + (y * y));
     }
 };
 
+/**
+ * Edge
+ */
+struct Edge {
+    Vertex* a;
+    Vertex* b;
+
+    Edge(Vertex* a, Vertex* b) : a(a),  b(b) { }
+    ~Edge() = default;
+};
+
+/**
+ * Triangle
+ */
 struct Triangle {
     Vertex* a;
     Vertex* b;
     Vertex* c;
 
+    Edge* edges[3]{ };
+
+
     Triangle() = default;
-    Triangle(Vertex* a, Vertex* b, Vertex* c) : a(a), b(b), c(c) { }
+    Triangle(Vertex* a, Vertex* b, Vertex* c, Edge* ab, Edge* bc, Edge* ca) : a(a), b(b), c(c) {
+        edges[0] = ab;
+        edges[1] = bc;
+        edges[2] = ca;
+    }
     ~Triangle() = default;
 
     [[nodiscard]]
-    HGE::Vector2d circumcenter() const {
+    HGE::Vector2f circumcenter() const {
 
         auto A = b->x - a->x;
         auto B = b->y - a->y;
@@ -53,19 +74,30 @@ struct Triangle {
             auto dx = (std::max({a->x, b->x, c->x}) - minx) * 0.5;
             auto dy = (std::max({a->y, b->y, c->y}) - miny) * 0.5;
 
-            return HGE::Vector2d(minx + dx, miny + dy);
+            return HGE::Vector2f(minx + dx, miny + dy);
         } else {
-            return HGE::Vector2d((D*E - B*F) / G, (A*F - C*E) / G);
+            return HGE::Vector2f((D*E - B*F) / G, (A*F - C*E) / G);
         }
     }
 
     [[nodiscard]]
-    HGE::Pointd circumcenterRadius() const {
+    HGE::Pointf circumcenterRadius() const {
         auto circum = circumcenter();
         auto dx = circum.x - a->x;
         auto dy = circum.y - a->y;
         return sqrt(dx * dx + dy * dy);
     }
+};
+
+/**
+ * Polygon
+ */
+struct Polygon {
+    std::vector<Edge*> edges{ };
+
+    /* todo: maybe unnesasary */
+    Polygon() = default;
+    ~Polygon() = default;
 };
 
 /**
@@ -75,7 +107,38 @@ struct Triangulation {
     constexpr static float sSuperTriangleOffset = 30.0;
 
     std::vector<std::unique_ptr<Vertex>> mVertices;
-    std::vector<Triangle> mTriangles;
+    std::vector<std::unique_ptr<Edge>> mEdges;
+    std::vector<std::unique_ptr<Triangle>> mTriangles;
+
+    Edge* getEdgeFromVerts(const Vertex* a, const Vertex* b) {
+        auto it = std::find_if(mEdges.begin(), mEdges.end(), [a, b] (auto & edge) {
+           return (edge->a == a && edge->b == b) || (edge->b == a && edge->a == b);
+        });
+
+        if( it != mEdges.end()) {
+            return it->get();
+        } else {
+            return nullptr;
+        }
+    }
+
+    void deleteTriangle(Triangle* triangle) {
+        auto it = std::find_if(mTriangles.begin(), mTriangles.end(), [&triangle] (const auto & tri) {
+            return tri.get() == triangle;
+        });
+        if(it != mTriangles.end()) {
+            mTriangles.erase(it);
+        }
+    }
+
+    void deleteEdge(Edge* edge) {
+        auto it = std::find_if(mEdges.begin(), mEdges.end(), [&edge] (const auto & e) {
+            return e.get() == edge;
+        });
+        if(it != mEdges.end()) {
+            mEdges.erase(it);
+        }
+    }
 
     static Vertex getFurthestPointOfVertices(const std::vector<Vertex> &vertices) {
         return *std::max_element(vertices.begin(), vertices.end(), [] (auto & a, auto & b) {
@@ -86,18 +149,26 @@ struct Triangulation {
     void createSuperTriangleFromFurthestPoint(Vertex & furthest) {
         const static double adjacent = sqrt(3);
         const static double height = adjacent/2;
-        auto radius = furthest.magnitude(); // + sSuperTriangleOffset;
+        auto radius = furthest.magnitude() + sSuperTriangleOffset;
 
-        auto bottomRight = mVertices.emplace_back(std::make_unique<Vertex>(radius * adjacent, -radius)).get();
-        auto bottomLeft = mVertices.emplace_back(std::make_unique<Vertex>(-(radius * adjacent), -radius)).get();
+        auto vertA = mVertices.emplace_back(std::make_unique<Vertex>(-(radius * adjacent), -radius)).get();
+        auto vertB = mVertices.emplace_back(std::make_unique<Vertex>(radius * adjacent, -radius)).get();
+        auto vertC = mVertices.emplace_back(std::make_unique<Vertex>( 0.0f , ((radius * 2) + (radius * adjacent)) * 0.5)).get();
 
-        auto top = mVertices.emplace_back(std::make_unique<Vertex>( 0.0f , ((radius * 2) + (radius * adjacent)) * 0.5))
-                .get();
-        mTriangles.emplace_back(Triangle(bottomLeft, bottomRight, top));
+        auto edgeAB = mEdges.emplace_back(std::make_unique<Edge>(vertA, vertB)).get();
+        auto edgeBC = mEdges.emplace_back(std::make_unique<Edge>(vertB, vertC)).get();
+        auto edgeCA = mEdges.emplace_back(std::make_unique<Edge>(vertC, vertA)).get();
+
+        mTriangles.emplace_back(std::make_unique<Triangle>(vertA, vertB, vertC, edgeAB, edgeBC, edgeCA));
     }
 
+    /**
+     *
+     * @param points
+     */
     void triangulate(const std::vector<HGE::Vector2f>& points) {
         mTriangles.clear();
+        mEdges.clear();
         mVertices.clear();
 
         auto vertices = std::vector<Vertex>();
@@ -105,37 +176,125 @@ struct Triangulation {
             return Vertex(point.x, point.y);
         });
 
-       auto radius = getFurthestPointOfVertices(vertices);
-       createSuperTriangleFromFurthestPoint(radius);
+        /** create the super triangle from the furthest point */
+        auto radius = getFurthestPointOfVertices(vertices);
+        createSuperTriangleFromFurthestPoint(radius);
+
+        /** for each vertex */
+        for(const auto & vert: vertices) {
+            auto badTriangles = std::vector<Triangle*>();
+
+            /** check to see if the point is inside the circumcenter of this triangle */
+           for(auto & triangle : mTriangles) {
+               if(HGE::isPointInACircle({vert.x, vert.y}, triangle->circumcenter(), triangle->circumcenterRadius())) {
+                   badTriangles.push_back(triangle.get());
+               }
+           }
+
+           auto polygon = Polygon();
+           auto edgesToDelete = std::vector<Edge*>();
+           /** for each triangle. check if it shares a an edge with another triangle */
+           for(auto & triangle : badTriangles) {
+
+                for(auto & edge : triangle->edges) {
+                    bool isNotShared = true;
+
+                    for(auto & badTriangle : badTriangles) {
+
+                        if(badTriangle != triangle) {
+
+                            if(badTriangle->edges[0] == edge ||
+                                badTriangle->edges[1] == edge ||
+                                badTriangle->edges[2] == edge) {
+                                isNotShared = false;
+                            }
+
+                        }
+                    }
+
+                    if(isNotShared) {
+                        polygon.edges.push_back(edge);
+                    } else {
+
+                        auto it = std::find(edgesToDelete.begin(), edgesToDelete.end(), edge);
+
+                        if(it == edgesToDelete.end()) {
+                            edgesToDelete.push_back(edge);
+                        }
+                    }
+                }
+           }
+
+           /** remove bad triangles */
+           for(auto & triangle : badTriangles) {
+               deleteTriangle(triangle);
+           }
+
+            for(auto & edge : edgesToDelete) {
+                deleteEdge(edge);
+            }
+
+
+           /** for each edge, add a new triangle to the new vertex */
+           // TODO: Rejig to make sure triangles are counter clockwise??
+            auto newVert = mVertices.emplace_back(std::make_unique<Vertex>(vert.x, vert.y)).get();
+            for(auto & edge : polygon.edges) {
+
+                Edge* edgeNA;
+                auto existingEdgeNA = getEdgeFromVerts(newVert, edge->a);
+                if(existingEdgeNA != nullptr) {
+                    edgeNA = existingEdgeNA;
+                } else {
+                    mEdges.emplace_back(std::make_unique<Edge>(newVert, edge->a));
+                    edgeNA = mEdges.back().get();
+                }
+
+                Edge* edgeBN;
+                auto existingEdgeBN = getEdgeFromVerts(edge->b, newVert);
+                if(existingEdgeBN != nullptr) {
+                    edgeBN = existingEdgeBN;
+                } else {
+                    mEdges.emplace_back(std::make_unique<Edge>(edge->b, newVert));
+                    edgeBN = mEdges.back().get();
+                }
+
+                mTriangles.emplace_back(std::make_unique<Triangle>(newVert, edge->a, edge->b, edgeNA, edge, edgeBN));
+            }
+
+
+        }
+
+        /* get all triangles linked to first 3 verts and delete them */
 
 
 
-//        radius = get furthest point();
-//        superTriangle = radiusToTriangle();
-//
-//        triangulation.add_triangle(superTriangle);
-//
-//        for(auto & vertex : mVertices) {
-//            std::vector<triangle> badTriangles{ };
-//
-//            for(auto & triangle : triangulation.triangles) {
-//
-//                if(vertexIsInTriangleCircumfrence(vertex, triangle)) {
-//                    badTriangles.push_back(triangle);
-//                }
-//
-//
-//
-//
-//
-//
-//            }
-//
-//
-//
-//
-//
-//        }
+
+        mTriangles.erase(std::remove_if(mTriangles.begin(), mTriangles.end(), [&] (const auto & triangle) {
+            return triangle->a == mVertices.at(0).get()
+                || triangle->a == mVertices.at(1).get()
+                || triangle->a == mVertices.at(2).get()
+                || triangle->b == mVertices.at(0).get()
+                || triangle->b == mVertices.at(1).get()
+                || triangle->b == mVertices.at(2).get()
+                || triangle->c == mVertices.at(0).get()
+                || triangle->c == mVertices.at(1).get()
+                || triangle->c == mVertices.at(2).get();
+        }), mTriangles.end());
+
+        /* get all edges linked to first 3 verts and delete them */
+        mEdges.erase(std::remove_if(mEdges.begin(), mEdges.end(), [&] (const auto & edge) {
+            return edge->a == mVertices.at(0).get()
+                   || edge->a == mVertices.at(1).get()
+                   || edge->a == mVertices.at(2).get()
+                   || edge->b == mVertices.at(0).get()
+                   || edge->b == mVertices.at(1).get()
+                   || edge->b == mVertices.at(2).get();
+        }), mEdges.end());
+
+        /* delete first 3 verts */
+        mVertices.erase(mVertices.begin() + 2);
+        mVertices.erase(mVertices.begin() + 1);
+        mVertices.erase(mVertices.begin() + 0);
 
     }
 };
--- include/dungeon_generator.h
+++ include/dungeon_generator.h
@@ -63,6 +63,7 @@ class DungeonGenerator {
     }
 
     /* */
+    /* todo: still sometimes fails, max iteration cap? before reset? or better method? */
     void separateRooms() {
         bool overlapsExist = true;
         while(overlapsExist) {
@@ -140,14 +141,13 @@ public:
             LOG_DEBUG("Dungeon Generator",
                     "id: ",
                     mRooms.back().mId,
-                    " generated point- size x: ",
-                      mRooms.back().mRect.mSize.x,
-                    " size y: ",
-                      mRooms.back().mRect.mSize.y,
-                    " pos x: ",
+                    " generated point- pos x: ",
                       mRooms.back().mRect.mPosition.x,
                       " pos y: ",
-                      mRooms.back().mRect.mPosition.y);
+                      mRooms.back().mRect.mPosition.y,
+                      "midpoint: ",
+                      mRooms.back().mRect.midpoint().x,
+                      mRooms.back().mRect.midpoint().y)
         }
 
         /* move the rooms so they do not overlap */
@@ -160,33 +160,58 @@ public:
 
         /* vector of midpoints */
         auto midpoints = std::vector<HGE::Vector2f>();
-        std::transform(mRooms.begin(), mRooms.end(), std::back_inserter(midpoints), [] (const auto & room) {
+        std::transform(mRooms.begin(), mRooms.end(), std::back_inserter(midpoints), [&] (const auto & room) {
             return room.mRect.midpoint();
         });
 
+
+
+
         /* do a delaneay run on rooms and generate a graph? */
         auto delaunay = Triangulation();
+        auto testVec = std::vector<HGE::Vector2f>();
+        testVec.emplace_back(-30.0f, -30.0f );
+        testVec.emplace_back(50.0f, 100.0f );
+        testVec.emplace_back(-50.0f, -100.0f );
+        testVec.emplace_back(20.0f, 150.0f );
+        testVec.emplace_back(-30.0f, 60.0f );
+        testVec.emplace_back(-120.0f, -100.0f );
+        testVec.emplace_back(90.0f, -150.0f );
+
+
         delaunay.triangulate(midpoints);
 
 
         for(auto const & triangle : delaunay.mTriangles) {
-            mDebug->drawLine(HGE::Vector2f ( 400 + triangle.a->x, 300 + triangle.a->y ),
-                             HGE::Vector2f ( 400 + triangle.b->x, 300 + triangle.b->y ), 10.0f );
+            mDebug->drawLine(HGE::Vector2f ( 400 + triangle->a->x, 300 + triangle->a->y ),
+                             HGE::Vector2f ( 400 + triangle->b->x, 300 + triangle->b->y ), 10.0f, {255, 255, 255} );
 
-            mDebug->drawLine(HGE::Vector2f ( 400 + triangle.b->x, 300 + triangle.b->y ),
-                             HGE::Vector2f ( 400 + triangle.c->x, 300 + triangle.c->y ), 10.0f );
+            mDebug->drawLine(HGE::Vector2f ( 400 + triangle->b->x, 300 + triangle->b->y ),
+                             HGE::Vector2f ( 400 + triangle->c->x, 300 + triangle->c->y ), 10.0f, {255, 255, 255});
 
-            mDebug->drawLine(HGE::Vector2f ( 400 + triangle.c->x, 300 + triangle.c->y ),
-                             HGE::Vector2f ( 400 + triangle.a->x, 300 + triangle.a->y ), 10.0f );
+            mDebug->drawLine(HGE::Vector2f ( 400 + triangle->c->x, 300 + triangle->c->y ),
+                             HGE::Vector2f ( 400 + triangle->a->x, 300 + triangle->a->y ), 10.0f, {255, 255, 255} );
 
-            mDebug->drawCircle(HGE::Vector2f ( triangle.circumcenter().x + 400.0f , triangle.circumcenter().y + 300.0f ),
-                    triangle.circumcenterRadius(), 10.0f);
-
+//            mDebug->drawCircle(HGE::Vector2f ( triangle->circumcenter().x + 400.0f , triangle->circumcenter().y + 300.0f ),
+//                    triangle->circumcenterRadius(), 10.0f, {255, 0, 0});
         }
 
-        LOG_DEBUG("Trangulation!", "circumcenter",
-                delaunay.mTriangles.begin()->circumcenter().x,
-                delaunay.mTriangles.begin()->circumcenter().y)
+//        for(auto const & edge : delaunay.mEdges) {
+//            mDebug->drawLine(HGE::Vector2f ( 400 + edge->a->x, 300 + edge->a->y ),
+//                             HGE::Vector2f ( 400 + edge->b->x, 300 + edge->b->y ), 10.0f );
+//        }
+
+        float index = 0.0f;
+        float step = 1.0f / delaunay.mVertices.size();
+        for(auto const & vert : delaunay.mVertices) {
+            mDebug->drawCircle(HGE::Vector2f ( vert->x + 400.0f , vert->y + 300.0f ),
+                    10.0f, 10.0f, {(step * index), 1.0f - (step * index), 0});
+            index += 1.0f;
+        }
+
+//        LOG_DEBUG("Trangulation!", "circumcenter",
+//                delaunay.mTriangles.begin()->circumcenter().x,
+//                delaunay.mTriangles.begin()->circumcenter().y)
 
 
         /* do a minimum angle check on graph? */
